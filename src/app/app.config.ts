253f9b39a9e4074e86d128c32bab6b4b362bb867
Riedmann Bastian <Riedmann.Basti@gmail.com>
2024-08-05 23:53:59 +0200

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"memoflow-65786","appId":"1:138143599983:web:86b847ff63810ef84c2c9a","storageBucket":"memoflow-65786.appspot.com","apiKey":"AIzaSyDHZqPuJ3PZ3auol1Gu_XXoTr5z1bbPRb4","authDomain":"memoflow-65786.firebaseapp.com","messagingSenderId":"138143599983"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())]
+};
